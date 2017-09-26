638145fde1d7f02d637c2545fe292e04ee6251ff
Johnathan Pagnutti <jpagnutti@Johnathans-MacBook-Pro.local>
2017-09-26 11:43:15 -0700

    Adding in several example files, WIP

---
+++ examples/p5example/sketch.js
@@ -0,0 +1,23 @@
+var prod;
+var prod_rdy
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  prod = new Productionist.default("faeMemoryLibrary", "http://localhost:8080/python/faeMemoryLibrary");
+  prod_rdy = prod.finalize();
+}
+
+function draw() {
+  prod_rdy.then(productionist => {
+    var contentRequest = {
+      'mustHave': new Set(["tags:party"]),
+      'mustNotHave': new Set(["tags:fire"]),
+      'scoringMetric': []
+    }
+
+    var output = productionist.fulfillContentRequest(contentRequest)
+    textSize(32);
+    text(output.toString(), 10, 30, 800, 600);
+    //console.log(output)
+  });
+}
